8592e6b2ba2ed0c069eb1af08281f6292d7af18f
Deany Dean <deanydean@shells>
2014-07-25 23:36:23 +0100

    Fixed up capsule webservice
    Added capsule test sim

--- src/main/groovy/gis/mc/services/web.groovy
+++ src/main/groovy/gis/mc/services/web.groovy
@@ -1,16 +1,72 @@
 package bot.services
 
 import bot.Bot
+import bot.comm.Comm
 import bot.control.Service
+import bot.services.GraphService
+
+import java.util.concurrent.*
 
 @Grab("com.sparkjava:spark-core:2.0.0")
-import spark.Spark
+import spark.*
+
+@Grab("com.google.code.gson:gson:2.2.4")
+import com.google.gson.Gson
 
 /**
  * The mission control web service
  */
 class WebService extends Service {
-    
+  
+    private static final END_OBJECT = new Object()
+
+    def dataHandler = new JsonTransformer()
+
+    def getCapsuleData = { req, res ->
+        try{
+            def dataQueue = new LinkedBlockingDeque()
+            
+            // Ask for all the data
+            new Comm("srv.graph.read")
+                .set(GraphService.NODE_LABEL, "Location")
+                .publish({ comm ->
+                    LOG.debug "Got ${comm.get(GraphService.OP_RESULT).size()}"
+                    comm.get(GraphService.OP_RESULT).each { dataQueue << it }
+                    dataQueue << END_OBJECT
+                })
+   
+            // Convert the queue into a list for the response
+            def response = []
+            while(true){
+                def obj = dataQueue.take()
+                if(obj == END_OBJECT) break
+                LOG.debug "Adding ${obj} to response"
+                response << obj
+            }
+
+            // Return the list
+            LOG.debug "Done! Returning ${response}"
+            def result = dataHandler.render(response)
+            return result
+        }catch(Throwable t){
+            LOG.error "Failed to get capsule data ${t}"
+            Spark.halt(500, "<img src=\"crap.png\"/> Ah crap!")
+        }
+    }
+
+    def addCapsuleData = { req, res ->
+        try{
+            def props = dataHandler.derenderToMap(req.body())
+            new Comm("srv.graph.create")
+                .set(GraphService.NODE_LABEL, "Location")
+                .set(GraphService.NODE_PROPS, props)
+                .publish()
+        }catch(e){
+            LOG.error "Failed to add capsule data ${e}"
+        }
+        return "{}"
+    }
+
     public WebService(){
         super("web", true, 9)
     }
@@ -21,17 +77,44 @@ class WebService extends Service {
             // Show server status
             return "STATUS NOT IMPLEMENTED YET"
         })
-        Spark.get("/capsule", { req, resp ->
-            // The capsule webservice interface
-            return "CAPSULE WEBSERVICE NOT IMPLEMENTED YET"
-        })
         Spark.get("/ping", { req, resp ->
             return "pong"
         })
+
+        // The capsule webservices
+        Spark.get("/capsule", "application/json", { req, res -> 
+            return getCapsuleData(req, res)})
+        Spark.post("/capsule", { req, res -> 
+            return addCapsuleData(req, res)})
     }
     
     @Override
     public void onStop(){
         Spark.stop()
+    }
+
+    // Data conversion (should probably be in core)
+    public class JsonTransformer implements ResponseTransformer {
+        
+        private Gson gson = new Gson()
+
+        @Override
+        public String render(model){
+            WebService.this.LOG.debug "Rendering ${model} to json"
+            def response = new StringBuilder("{\n")
+            model.each { entry -> 
+                response << "{ \"id\": \"${entry.key}\", "
+                entry.value.each { k, v ->
+                    response << "\"${k}\": \"${v}\", " 
+                }
+                response << " },\n"
+            }
+            return "${response}}"
+        }
+
+        public Map derenderToMap(String json){
+            return gson.fromJson(json, Map.class)
+        }
+
     }
 }
---
+++ src/test/groovy/gis/test/ops/run/capsule/sim.groovy
@@ -0,0 +1,50 @@
+#!/usr/bin/groovy
+// Run a capsule simulation
+// usage: sim <mc-url>
+package gis.test.ops
+
+import bot.util.TimeTool
+
+@Grab("com.google.code.gson:gson:2.2.4")
+import com.google.gson.Gson
+
+@Grab("org.codehaus.groovy.modules.http-builder:http-builder:0.7")
+import groovyx.net.http.AsyncHTTPBuilder
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+
+def url = args[0]
+
+// Some bits to help with the sim
+def rng = new Random(10111100)
+def pos = [ rng.nextFloat(), rng.nextFloat(), rng.nextFloat()*100 ]
+def timer = new TimeTool()
+def http = new AsyncHTTPBuilder(poolSize: 4, uri: url, timeout: 1000)
+def nextFloat = { c, r -> return c+(r-(rng.nextFloat()*(r*2))) } 
+def normalise = { n, hi, low -> return (n>hi) ? hi : (n<low) ? low : n }
+def move = 0.001
+
+BOT.on("gis.sim.post", { comm ->
+    // Update values, movement in range, always going up
+    pos = [
+        normalise(nextFloat(pos[0], move), 90, -90),
+        normalise(nextFloat(pos[1], move), 90, -90),
+        pos[2]+rng.nextFloat()*move
+    ]
+
+    LOG.info "Updating position to ${pos}"
+    http.request(POST, JSON){
+        body = [ "long": pos[0], "lat": pos[1], "alt": pos[2] ]
+    
+        response.success = { resp, json ->
+            LOG.info "Done: $json"
+        }
+    }
+
+
+})
+
+LOG.info "Simulating capsule updates to $url"
+timer.interval({ BOT.comm "gis.sim.post" }, 1);
+
+BOT.join()
